9f4fbbb613c177ef56fdb129d213d9bc70952817
LucasMonteras <lucasmonteras@gmail.com>
2023-05-31 20:06:18 -0300

    Entrega Parcial

--- Parcial front/src/App.jsx
+++ Parcial front/src/App.jsx
@@ -1,23 +1,53 @@
 
+import { useState } from 'react'
 import './App.css'
 import Card from './Componentes/Card'
 
 function App() {
   
+  const [estudiante,setEstudiante] = useState({
+    nombre:"",
+    apellido:"",
+    materia:""
+  })
+
+  const [show, setShow] = useState(false)
+  const [error,setError] = useState(false)
+
+  // condiciones
+
+const handleSubmit =(e)=>{
+  e.preventDefault()
+  if(estudiante.nombre.length > 3 && estudiante.apellido.length>3 && estudiante.materia.length>6){
+    setShow(true)
+    setError(false)
+  }else{
+    setError(true)
+  }
 
 
-
+}
 
   return (
     <div className='App' style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
-      <h1>Carga de todos Estudiantes</h1>
-      <Card/>
-      <form style={{
+      <h1>Cargar Estudiante </h1>
+      
+      <form onSubmit={handleSubmit} style={{
           display:'flex',
           flexDirection:'column',
           width :'200px',
-          alignItems:'center'}}>
+          alignItems:'center',
+          }}>
+            <label></label>
+            <input type='text' onChange={(e) => setEstudiante((prevState) => ({...prevState, nombre: e.target.value}))} placeholder='Nombre del estudiante' style={{marginBottom: '10px'}}/>
+            <label></label>
+            <input type='text' onChange={(e) => setEstudiante((prevState) => ({...prevState, apellido: e.target.value}))} placeholder='Apellido del estudiante'style={{marginBottom: '10px'}}/>
+            <label></label>
+            <input type='text' onChange={(e) => setEstudiante((prevState) => ({...prevState, materia: e.target.value}))} placeholder='Materia a rendir 'style={{marginBottom: '20px'}}/>
+            <button style={{marginBottom: '40px'}} >Enviar</button >
+            {error&& 'Por favor chequea que la información sea correcta'}
       </form>
+      {show && <Card estudiante={estudiante}/>}
     </div>
   )
 }
--- Parcial front/src/Componentes/Card.jsx
+++ Parcial front/src/Componentes/Card.jsx
@@ -1,18 +1,23 @@
 
 import cardStyles from './Card.module.css'
+import memeJava from '../assets/memeJava.jpg'
 
 
 
-
-const Card = () => {
+const Card = ({estudiante}) => {
 
 
 
     return (
-        <div className={cardStyles.card}>
-            <h3>Hola, soy una tarjeta</h3>
+        <div className={cardStyles.card} style={{display:'flex',flexDirection:'column',justifyContent:'center' }}>
+            <h3>Nombre : {estudiante.nombre}</h3>
+            <h3>Apellido: {estudiante.apellido}</h3>
+            <img className='card-img' src={memeJava}/>
+            <h4>Materia: {estudiante.materia}</h4>
         </div>
     )
 }
 
 export default Card
+
+
